532c55c3bdfc902c6fb7be5a2de288f186eed6b5
euro <phuchit@omg.network>
2020-11-08 21:31:44 +0700

    Refactoring tests and docs

--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,7 @@
 export {};
 
-import { ETH } from "./constants";
 import Web3 from "web3";
+import { ETH } from "./constants";
 import { getNetwork } from "@ethersproject/networks";
 import { getDefaultProvider, BaseProvider } from "@ethersproject/providers";
 import { getToken, getPair, getRoute, getTrade } from "./util";
@@ -25,12 +25,14 @@ export default class Uniswap {
   swap() {}
 
   /**
-   * Get the price for given token comparing with another token (currency).
-   * @param tokenAddress An address for token you want to know price.
-   * @param currencyAddress (optional) An address for token used for determine price.
+   * Get the price for the given token by comparing with another token (currency).
+   * @param tokenAddress A token address you want to know price.
+   * @param currencyAddress (optional) A token address to be used as currency.
    * @param amount (optional) An amount of token to calculate the price.
    *
-   * @returns An amount of currency token.
+   * @returns A trade object.
+   * Then, it can be used to get token price against currency token. For example,
+   * Call `trade.outputAmount.toSignificant(6)` to get the price.
    */
   async getPrice(
     tokenAddress: string,
@@ -39,36 +41,34 @@ export default class Uniswap {
   ) {
     const isInputETH = tokenAddress === ETH;
     const isOutputETH = currencyAddress === ETH;
-    const inputToken = isInputETH
-      ? WETH[this.chainId]
-      : await getToken(
-          this.web3,
-          this.chainId,
-          Web3.utils.toChecksumAddress(tokenAddress)
-        );
-    const outputToken = isOutputETH
-      ? WETH[this.chainId]
-      : await getToken(
-          this.web3,
-          this.chainId,
-          Web3.utils.toChecksumAddress(currencyAddress)
-        );
+    const token = await getToken(
+      this.web3,
+      this.chainId,
+      Web3.utils.toChecksumAddress(tokenAddress)
+    );
+    const currencyToken = await getToken(
+      this.web3,
+      this.chainId,
+      Web3.utils.toChecksumAddress(currencyAddress)
+    );
+    const inputToken = isInputETH ? WETH[this.chainId] : token;
+    const outputToken = isOutputETH ? WETH[this.chainId] : currencyToken;
     let route;
     if (isInputETH || isOutputETH) {
-      const inputPair = await getPair(inputToken, outputToken, this.provider);
-      route = getRoute([inputPair], inputToken);
+      const pair1 = await getPair(inputToken, outputToken, this.provider);
+      route = getRoute([pair1], inputToken);
     } else {
-      const inputPair = await getPair(
+      const pair1 = await getPair(
         inputToken,
         WETH[this.chainId],
         this.provider
       );
-      const outputPair = await getPair(
+      const pair2 = await getPair(
         WETH[this.chainId],
         outputToken,
         this.provider
       );
-      route = getRoute([inputPair, outputPair], inputToken);
+      route = getRoute([pair1, pair2], inputToken);
     }
     const trade = getTrade(amount, inputToken, route);
     return trade.outputAmount.toSignificant(6);
--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -1,6 +1,9 @@
 import Web3 from "web3";
 import UniswapManager from "../src";
+import { ETH } from "../src/constants";
 import { JsonRpcProvider } from "@ethersproject/providers";
+
+const TIMEOUT = 10000;
 
 describe("Uniswap Manager", function () {
   let manager;
@@ -11,13 +14,39 @@ describe("Uniswap Manager", function () {
     manager = new UniswapManager(web3, chainId, new JsonRpcProvider(url));
   });
 
-  it("can get token price", function () {
-    const YFI = "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e";
-    const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+  describe("getPrice", function () {
+    it("can get token price for erc20/erc20 pair", function () {
+      const YFI = "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e";
+      const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
 
-    return manager.getPrice(YFI, USDT, (10 ** 18).toString()).then((result) => {
-      console.log(result);
-      expect(result).toBeDefined();
-    });
-  }, 15000);
+      return manager
+        .getPrice(YFI, USDT, (10 ** 18).toString())
+        .then((result) => {
+          console.log(result);
+          expect(result).toBeDefined();
+        });
+    }, TIMEOUT);
+
+    it("can get token price for erc20/eth pair", function () {
+      const YFI = "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e";
+
+      return manager
+        .getPrice(YFI, ETH, (10 ** 18).toString())
+        .then((result) => {
+          console.log(result);
+          expect(result).toBeDefined();
+        });
+    }, TIMEOUT);
+
+    it("can get token price for eth/erc20 pair", function () {
+      const YFI = "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e";
+
+      return manager
+        .getPrice(ETH, YFI, (10 ** 18).toString())
+        .then((result) => {
+          console.log(result);
+          expect(result).toBeDefined();
+        });
+    }, TIMEOUT);
+  });
 });
